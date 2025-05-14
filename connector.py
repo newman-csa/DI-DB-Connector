93467f9f70901c31613914532afcc181e9fc1a27
newman-csa <146094244+newman-csa@users.noreply.github.com>
2025-05-14 13:15:47 -0400

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,7 +1,11 @@
 import mysql.connector
-from termcolor import colored
+from termcolor import cprint, colored
 
-# Set password
+# pip install mysql-connector-python
+# pip install termcolor
+# pip install tabulate
+
+#! Set password
 def open_connection():
     connection = mysql.connector.connect(user='logann24',
                                         password='',
@@ -21,11 +25,32 @@ def query_connection(query):
    return data
 
 
-id = input(colored('Enter Student ID:', 'red', attrs=['bold']) + '\n> ')
-records = query_connection(f"CALL student_schedule_by_student_id({id})")
+def print_teacher_schedule(id):
+    records = query_connection(f"CALL teacher_schedule_by_teacher_id({id})")
 
-for row in records:
-  print(f'\nPeriod: {row[1]}')
-  print(f'Course: {row[2]}')
-  print(f'Room: {row[3]}')
-  print(f'Teacher: {row[4]} {row[5]}')
+    for row in records:
+        print(f'\nPeriod: {row[1]}')
+        print(f'Course: {row[2]}')
+        print(f'Room: {row[3]}')
+
+
+def print_student_schedule(id):
+    records = query_connection(f"CALL student_schedule_by_student_id({id})")
+
+    for row in records:
+        print(f'\nPeriod: {row[1]}')
+        print(f'Course: {row[2]}')
+        print(f'Room: {row[3]}')
+        print(f'Teacher: {row[4]} {row[5]}')
+
+cprint('(T)eacher or (S)tudent', 'red', attrs=['bold'])
+student_or_teacher = input('> ').lower()
+
+cprint('\nEnter ID:', 'red', attrs=['bold'])
+id = input('> ')
+
+if student_or_teacher == 's':
+    print_student_schedule(id)
+elif student_or_teacher == 't':
+    print_teacher_schedule(id)
+
